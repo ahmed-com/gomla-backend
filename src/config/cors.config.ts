c5228485377723cf5e37e72178e578d62ed2c81c
ahmed-com <ahmed0grwan@gmail.com>
2022-02-05 17:20:59 +0200

    added basic CORS configuration

---
+++ src/config/cors.config.ts
@@ -0,0 +1,1 @@
+export default {origin: 'http://localhost:8080', credentials: true}
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import corsConfig from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist : true 
   }));
-  // TO-DO: configure CORS
+  app.enableCors(corsConfig)
   app.use(cookieParser());
   await app.listen(3000);
 }
